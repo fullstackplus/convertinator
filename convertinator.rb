3da7bb66172e1c4f94c78b4ffdc91c8b0367ab6d
vahagnhay <vahagn@fullstackplus.tech>
2020-06-11 10:19:11 +0200

    fixed traverse_and_print; now sorts directory contents numerically

--- convertinator.rb
+++ convertinator.rb
@@ -8,17 +8,18 @@ RENDERER = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tabl
 OUTPUTDIR = '/build'
 
 module Convertinator
+  extend self
 
-  def self.output_path(filename)
+  def output_path(filename)
    File.join(Dir.pwd, OUTPUTDIR, filename)
   end
 
-  def self.buildfile_path(filename)
+  def buildfile_path(filename)
    File.join(Dir.pwd, OUTPUTDIR, 'lib', filename)
   end
 
   # (str) -> nil
-  def self.create_file(outputpath)
+  def create_file(outputpath)
     unless File.file? outputpath
       File.open(outputpath, 'w') { |f| f.write '' }
     end
@@ -28,12 +29,12 @@ module Convertinator
   # followed by a single dash, followed by any text.
   #
   # (str) -> Regex | nil
-  def self.content?(path)
+  def content?(path)
     File.basename(path).match /^[0-9]+\-{1}\w/
   end
 
   # (str) -> bool
-  def self.markdown?(path)
+  def markdown?(path)
     File.file?(path) &&
     File.extname(path).downcase.eql?('.mdown')
   end
@@ -41,23 +42,31 @@ module Convertinator
   # Prototype method for traverse_and_merge. Prints only.
   #
   # (str, str) ->
-  def self.traverse_and_print(startdir, indent='')
-    Dir.foreach(startdir) do |filename|
-      path = File.join(startdir, filename)
+  def traverse_and_print(startdir, indent='')
+    entries = Dir.entries(startdir).sort
+    entries.each do |filename|
+      next if filename == "." or filename == ".."
 
-      if filename == "." or filename == ".."
-        next
-
-      elsif File.file?(path)
+      if File.file?(filename)
         puts [indent, filename].join
         next
 
-      else File.directory?(path)
-        puts [indent, path, "/"].join
-        traverse_and_print(path, [indent, '    '].join)
+      else File.directory?(filename)
+        puts [indent, filename, "/"].join
+        traverse_and_print(filename, [indent, '    '].join)
       end
     end
   end
+
+
+  def visit(startdir, indent='')
+     puts Dir.entries(startdir).sort
+     puts "HA"
+     puts Dir.glob("*.mdown").sort
+     puts "HAHA"
+     puts Dir.glob("underdir").sort
+  end
+
 
   # Performs a depth-first traversal of the directory tree
   # starting with the specified @startdir, merging .mdown
@@ -67,7 +76,7 @@ module Convertinator
   # @depth : for debugging
   #
   # (str, file, depth) -> nil
-  def self.traverse_and_merge(startdir, outputpath, indent="", depth=1)
+  def traverse_and_merge(startdir, outputpath, indent="", depth=1)
     Dir.foreach(startdir) do |filename|
       path = File.join(startdir, filename)
 
@@ -91,20 +100,20 @@ module Convertinator
     end
   end
 
-  def self.merge_markdown(startdir, outputfile="merged.mdown")
+  def merge_markdown(startdir, outputfile="merged.mdown")
     outputpath = output_path(outputfile)
-    self.create_file(outputpath)
-    self.traverse_and_merge(startdir, outputpath)
+    create_file(outputpath)
+    traverse_and_merge(startdir, outputpath)
   end
 
-  def self.convert_dir(startdir, inputfile="merged.mdown", outputfile="file.html")
+  def convert_dir(startdir, inputfile="merged.mdown", outputfile="file.html")
     merge_markdown(startdir, inputfile)
     input  = output_path(inputfile)
     output = output_path(outputfile)
     to_html(input, output)
   end
 
-  def self.convert_file(inputfile)
+  def convert_file(inputfile)
     markdown = File.basename inputfile
     filename = markdown.split('.')[0]
     # set name of outputfile to that of inputfile:
@@ -112,7 +121,7 @@ module Convertinator
     to_html(inputfile, output)
   end
 
-  def self.to_html(inputfile, outputpath)
+  def to_html(inputfile, outputpath)
     html = RENDERER.render(File.read(inputfile))
     File.open(outputpath, 'w') do |f|
       f.write(File.read(buildfile_path('header.html')))
